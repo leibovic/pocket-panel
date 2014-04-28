aa34d80dd9602cd434767607ee6a952c20216845
Margaret Leibovic <margaret.leibovic@gmail.com>
2014-04-28 14:35:04 -0700

    clean up code and use localized strings

--- bootstrap.js
+++ bootstrap.js
@@ -6,20 +6,52 @@ Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/Task.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-const PANEL_ID = "com.margaretleibovic.pocket";
-const DATASET_ID = "com.margaretleibovic.pocket.items";
-
 XPCOMUtils.defineLazyGetter(this, "Pocket", function() {
   let win = Services.wm.getMostRecentWindow("navigator:browser");
   Services.scriptloader.loadSubScript("chrome://pocketpanel/content/pocket.js", win);
   return win.Pocket;
 });
 
+XPCOMUtils.defineLazyGetter(this, "Strings", function() {
+  return Services.strings.createBundle("chrome://pocketpanel/locale/pocket.properties");
+});
+
+// Unique IDs for panel and dataset.
+const PANEL_ID = "pocket.panel@margaretleibovic.com";
+const DATASET_ID = "pocket.dataset@margaretleibovic.com";
+
+function optionsCallback() {
+  return {
+    title: Strings.GetStringFromName("panel.title"),
+    views: [{
+      type: Home.panels.View.LIST,
+      dataset: DATASET_ID,
+      onrefresh: function onrefresh() {
+        if (!Pocket.isAuthenticated) {
+          Pocket.authenticate(() => refreshDataset());
+        } else {
+          refreshDataset();
+        }
+      }
+    }],
+    auth: {
+      authenticate: function authenticate() {
+        Pocket.authenticate(function() {
+          Home.panels.setAuthenticated(PANEL_ID, true);
+          refreshDataset(openPocketPanel);
+        });
+      },
+      messageText: Strings.GetStringFromName("auth.message"),
+      buttonText: Strings.GetStringFromName("auth.button")
+    }
+  };
+}
+
 function openPocketPanel() {
   Services.wm.getMostRecentWindow("navigator:browser").BrowserApp.loadURI("about:home?panel=" + PANEL_ID);
 }
 
-function updateData(callback) {
+function refreshDataset(callback) {
   Pocket.getItems(function(list) {
     let items = [];
     for (let id in list) {
@@ -27,6 +59,7 @@ function updateData(callback) {
       items.push({
         title: item.resolved_title,
         description: item.excerpt,
+        // Open URLs in reader mode
         url: "about:reader?url=" + encodeURIComponent(item.resolved_url)
       });
     }
@@ -35,112 +68,31 @@ function updateData(callback) {
 }
 
 function saveItems(items, callback) {
-  HomeProvider.requestSync(DATASET_ID, function() {
-    Task.spawn(function() {
-      let storage = HomeProvider.getStorage(DATASET_ID);
-      yield storage.deleteAll();
-      yield storage.save(items);
-    }).then(callback, e => Cu.reportError("Error saving Pocket items to HomeProvider: " + e));
-  });
+  Task.spawn(function() {
+    let storage = HomeProvider.getStorage(DATASET_ID);
+    yield storage.deleteAll();
+    yield storage.save(items);
+  }).then(callback, e => Cu.reportError("Error saving Pocket items to HomeProvider: " + e));
 }
 
 function deleteItems() {
-  HomeProvider.requestSync(DATASET_ID, function() {
-    Task.spawn(function() {
-      let storage = HomeProvider.getStorage(DATASET_ID);
-      yield storage.deleteAll();
-    }).then(null, e => Cu.reportError("Error deleting Pocket items from HomeProvider: " + e));
-  });
-}
-
-var gMenuId;
-
-// Add a menu-item as a hack to force update data.
-function addMenuItem(window) {
-  gMenuId = window.NativeWindow.menu.add({
-    name: "Update Pocket panel",
-    parent: window.NativeWindow.menu.toolsMenuID,
-    callback: function() {
-      if (!Pocket.isAuthenticated) {
-        Pocket.authenticate(() => updateData(openPocketPanel));
-      } else {
-        updateData(openPocketPanel);
-      }
-    }
-  });
-}
-
-function removeMenuItem(window) {
-  window.NativeWindow.menu.remove(gMenuId);
+  Task.spawn(function() {
+    let storage = HomeProvider.getStorage(DATASET_ID);
+    yield storage.deleteAll();
+  }).then(null, e => Cu.reportError("Error deleting Pocket items from HomeProvider: " + e));
 }
 
 /**
  * bootstrap.js API
  */
-var windowListener = {
-  // Wait for the window to finish loading
-  onOpenWindow: function(aWindow) {
-    let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
-    domWindow.addEventListener("load", function() {
-      domWindow.removeEventListener("load", arguments.callee, false);
-      addMenuItem(domWindow);
-    }, false);
-  },
-  onCloseWindow: function(aWindow) {},
-  onWindowTitleChange: function(aWindow, aTitle) {}
-};
-
 function startup(aData, aReason) {
-  // Load menu item into any existing windows
-  let windows = Services.wm.getEnumerator("navigator:browser");
-  while (windows.hasMoreElements()) {
-    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-    addMenuItem(domWindow);
-  }
-
-  // Use a window listener to add a menu item to any new windows.
-  Services.wm.addListener(windowListener);
-
-  // Callback function that generates panel configuation
-  function optionsCallback() {
-    return {
-      title: "Pocket",
-      views: [{
-        type: Home.panels.View.LIST,
-        dataset: DATASET_ID,
-        onrefresh: function() {
-          if (!Pocket.isAuthenticated) {
-            Pocket.authenticate(() => updateData());
-          } else {
-            updateData();
-          }
-        }
-      }],
-      auth: {
-        authenticate: function authenticate() {
-          Pocket.authenticate(function() {
-            Home.panels.setAuthenticated(PANEL_ID, true);
-            updateData(openPocketPanel);
-          });
-        },
-        messageText: "Please log in to Pocket",
-        buttonText: "Log in"
-      }
-    };
-  }
-
-  // Always register a panel and a periodic sync listener.
+  // Always register panel on startup.
   Home.panels.register(PANEL_ID, optionsCallback);
-
-  HomeProvider.addPeriodicSync(DATASET_ID, 3600, updateData);
 
   switch(aReason) {
     case ADDON_ENABLE:
     case ADDON_INSTALL:
       Home.panels.install(PANEL_ID);
-
-      // Fetch items for the first time.
-      Pocket.authenticate(() => updateData(openPocketPanel));
       break;
 
     case ADDON_UPGRADE:
@@ -148,33 +100,23 @@ function startup(aData, aReason) {
       Home.panels.update(PANEL_ID);
       break;
   }
+
+  // Update data once every hour.
+  HomeProvider.addPeriodicSync(DATASET_ID, 3600, refreshDataset);
 }
 
 function shutdown(aData, aReason) {
   if (aReason == ADDON_UNINSTALL || aReason == ADDON_DISABLE) {
-    deleteItems();
     Home.panels.uninstall(PANEL_ID);
-    try {
-      Home.panels.setAuthenticated(PANEL_ID, false);
-    } catch (e) {}
+    deleteItems();
+
+    Home.panels.setAuthenticated(PANEL_ID, false);
     Pocket.clearAccessToken();
   }
 
   Home.panels.unregister(PANEL_ID);
-
-  // Remove menu item from any existing windows
-  let windows = Services.wm.getEnumerator("navigator:browser");
-  while (windows.hasMoreElements()) {
-    let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
-    removeMenuItem(domWindow);
-  }
-
-  // Stop listening for new windows
-  Services.wm.removeListener(windowListener);
 }
 
-function install(aData, aReason) {
-}
+function install(aData, aReason) {}
 
-function uninstall(aData, aReason) {
-}
+function uninstall(aData, aReason) {}
--- config_build.sh
+++ config_build.sh
@@ -3,11 +3,11 @@
 # Build config for the build script, build.sh. Look there for more info.
 
 APP_NAME=pocketpanel
-CHROME_PROVIDERS="content"
+CHROME_PROVIDERS="content locale"
 CLEAN_UP=1
 ROOT_FILES="bootstrap.js"
-ROOT_DIRS=
+ROOT_DIRS="locale"
 BEFORE_BUILD=
 AFTER_BUILD=
 PUSH_TO_DEVICE=1
-ANDROID_APP_ID=org.mozilla.fennec_leibovic
+ANDROID_APP_ID=org.mozilla.fennec
